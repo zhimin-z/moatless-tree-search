b5915be548c6e68f03bd30cbb0a45a4f4e29692f
Albert Örwall <albert@moatless.ai>
2024-10-23 11:25:46 +0200

    Tests

--- tests/benchmark/test_run_evaluation.py
+++ tests/benchmark/test_run_evaluation.py
@@ -1,13 +1,7 @@
-import logging
 import os
 
-from moatless.benchmark.evaluation import Evaluation, TreeSearchSettings
+from moatless.benchmark.evaluation import Evaluation, TreeSearchSettings, ModelSettings
 from moatless.benchmark.run_evaluation import evaluate_search_and_code
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
 
 repo_base_dir = "/tmp/repos"
 evaluations_dir = os.path.join(
@@ -15,25 +9,28 @@ evaluations_dir = os.path.join(
     "evaluations",
 )
 
-tree_search_settings = TreeSearchSettings(
-    max_expansions=3,
-    max_iterations=50,
-    min_finished_transitions=3,
-    max_finished_transitions=5,
-    reward_threshold=90,
-    provide_feedback=True,
-    debate=False,
-)
-
 
 def test_gpt4o_mini():
+    tree_search_settings = TreeSearchSettings(
+        model=ModelSettings(
+            model="gpt-4o-mini-2024-07-18",
+            temperature=0.7,
+        ),
+        max_expansions=3,
+        max_iterations=50,
+        min_finished_transitions=3,
+        max_finished_transitions=5,
+        provide_feedback=True,
+        best_first=True,
+        debate=False,
+    )
+
     evaluate_search_and_code(
         model="gpt-4o-mini-2024-07-18",
-        temperature=1.0,
-        instance_ids=["django__django-11039"],  # django_dataset,
+        temperature=0.7,
         max_transitions=30,
         tree_search_settings=tree_search_settings,
-        name="django",
-        evaluation_name="20240918_django_search_feedback",
+        evaluation_name="verify_gpt4o_mini",
         num_workers=1,
+        instance_ids=["django__django-15252"],
     )
